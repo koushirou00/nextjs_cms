aed30a396b5adbf379a568f7965dcca38f0554e2
koushirou00 <130426310+koushirou00@users.noreply.github.com>
2023-10-26 01:42:48 +0900

    軽微な修正

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
-
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/app/styles/scss')],
-  },
 }
 
 module.exports = nextConfig
